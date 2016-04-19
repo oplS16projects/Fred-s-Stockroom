c8b9e452fbe71ec85256eac2c66fc3b59b1b6483
Krin Yongvongphaiboon <book.krin@gmail.com>
2016-04-19 19:14:52 -0400

    Added files via upload
    Milestone 1

---
+++ FP3.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require web-server/formlets)
+(require web-server/servlet
+         web-server/servlet-env)
+
+
+ 
+(define (start req)
+ (response/xexpr
+   `(html (head (title "Fred's Stockroom")) ;Header
+          ;Page Title
+          (body (p "Fred's Stockroom"))
+          ;Form
+          (list
+ '(div "Stock1" (input ([name "input_0"])))
+ '(div "Stock2" (input ([name "input_1"]))))
+          ;Submit button
+  (button  "Submit"))))
+
+ 
+(serve/servlet start)
+
+
+
+
+ 
