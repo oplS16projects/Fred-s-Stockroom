46e268ffce0e3e4501e22099976aece90b7e8356
Krin Yongvongphaiboon <book.krin@gmail.com>
2016-04-25 14:34:07 -0400

    Updated Dispatch-URL

--- front-end.rkt
+++ front-end.rkt
@@ -1,22 +1,41 @@
-#lang web-server/insta
+#lang racket/base
+(require web-server/servlet)
 
-(require web-server/formlets)
-(require web-server/servlet
-         web-server/servlet-env)
+(require (planet jeeve/markets))
  
-(define (start req)
+(define (build-request-page stock1 stock2 k-url)
  (response/xexpr
    `(html (head (title "Fred's Stockroom")) ; Title
-          (link ((rel "stylesheet")
-                 (href "/static.css")
-                 (type "text/css")))
           ;Body Title
           (body (h3 "Fred's Stockroom"))
           ;Form
-          (list
- '(div "Stock 1:" (input ([name "input_0"])))
- '(div "Stock 2:" (input ([name "input_1"]))))
-          ;Submit button
-  (button  "Submit"))))
- 
-(static-files-path "cssfile")
+          
+          (form ([action ,k-url] [method "post"])
+                
+                      ;; First Stock Symbol
+                      "Enter " ,stock1 " stock symbol to compare: "
+                      (input ([type "text"] [name "character"] [value ""]))
+                      ;; Second Stock symbol
+                      (p "Enter " ,stock2 " stock symbol to compare: " 
+                      (input ([type "text"] [name "character"] [value ""])))
+                      ;; Submit button
+                      (input ([type "submit"] [name "enter"] [value "Enter"]))))))
+
+
+(define (get-first req)
+  (build-request-page "first" "second" (add-url display-data)))
+
+
+(define (display-data req)
+  (response/xexpr
+   `(html (head (title "Result"))
+          (body ([bgcolor "white"])
+                (p "Stocks Data: ")))))
+
+
+(define-values (start add-url)
+  (dispatch-rules
+   [("display-data") #:method "post"  display-data]
+   [else get-first]))
+
+(serve/dispatch start)
